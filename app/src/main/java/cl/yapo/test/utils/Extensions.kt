4982d13a4e7f61884d5a02468373b2cd501544ed
gdavidpb <gerardoprado93@gmail.com>
2019-04-11 21:32:10 -0400

    extensions added

---
+++ app/src/main/java/cl/yapo/test/utils/Extensions.kt
@@ -0,0 +1,70 @@
+package cl.yapo.test.utils
+
+import android.net.ConnectivityManager
+import androidx.lifecycle.MutableLiveData
+import cl.yapo.test.domain.usecase.coroutines.Completable
+import cl.yapo.test.domain.usecase.coroutines.Result
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.HttpException
+import retrofit2.Response
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+/* Context */
+
+fun ConnectivityManager.isNetworkAvailable() = activeNetworkInfo?.isConnected == true
+
+/* Live data */
+
+typealias LiveResult<T> = MutableLiveData<Result<T>>
+typealias LiveCompletable = MutableLiveData<Completable>
+
+/* LiveResult */
+
+@JvmName("postCompleteResult")
+fun <T> LiveResult<T>.postSuccess(value: T) = postValue(Result.OnSuccess(value))
+
+@JvmName("postThrowableResult")
+fun <T> LiveResult<T>.postThrowable(throwable: Throwable) = postValue(Result.OnError(throwable))
+
+@JvmName("postLoadingResult")
+fun <T> LiveResult<T>.postLoading() = postValue(Result.OnLoading())
+
+@JvmName("postCancelResult")
+fun <T> LiveResult<T>.postCancel() = postValue(Result.OnCancel())
+
+@JvmName("postEmptyResult")
+fun <T> LiveResult<T>.postEmpty() = postValue(Result.OnEmpty())
+
+/* LiveCompletable */
+
+@JvmName("postCompleteCompletable")
+fun LiveCompletable.postComplete() = postValue(Completable.OnComplete)
+
+@JvmName("postThrowableCompletable")
+fun LiveCompletable.postThrowable(throwable: Throwable) = postValue(Completable.OnError(throwable))
+
+@JvmName("postLoadingCompletable")
+fun LiveCompletable.postLoading() = postValue(Completable.OnLoading)
+
+@JvmName("postCancelCompletable")
+fun LiveCompletable.postCancel() = postValue(Completable.OnCancel)
+
+/* Coroutines */
+
+suspend fun <T> Call<T>.await() = suspendCoroutine<T?> { continuation ->
+    enqueue(object : Callback<T?> {
+        override fun onResponse(call: Call<T?>, response: Response<T?>) {
+            if (response.isSuccessful)
+                continuation.resume(response.body())
+            else
+                continuation.resumeWithException(HttpException(response))
+        }
+
+        override fun onFailure(call: Call<T?>, t: Throwable) {
+            continuation.resumeWithException(t)
+        }
+    })
+}
