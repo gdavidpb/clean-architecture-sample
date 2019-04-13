e285a00cd715f1835a4d1b5638e1f6c7dfb605b7
gdavidpb <gerardoprado93@gmail.com>
2019-04-12 22:15:02 -0400

    api koin test

---
+++ app/src/test/java/cl/yapo/test/TestModule.kt
@@ -0,0 +1,35 @@
+package cl.yapo.test
+
+import cl.yapo.test.data.source.apple.iTunesSearchApi
+import cl.yapo.test.utils.URL_BASE_ITUNES_SEARCH_API
+import okhttp3.OkHttpClient
+import org.koin.dsl.module.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val testModule = module {
+    /* Retrofit */
+
+    single {
+        OkHttpClient.Builder()
+            .callTimeout(1, TimeUnit.MINUTES)
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(1, TimeUnit.MINUTES)
+            .writeTimeout(1, TimeUnit.MINUTES)
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(URL_BASE_ITUNES_SEARCH_API)
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    single {
+        get<Retrofit>()
+            .create(iTunesSearchApi::class.java) as iTunesSearchApi
+    }
+}
---
+++ app/src/test/java/cl/yapo/test/iTunesApiTest.kt
@@ -0,0 +1,72 @@
+package cl.yapo.test
+
+import cl.yapo.test.data.source.apple.iTunesSearchApi
+import cl.yapo.test.utils.await
+import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.koin.standalone.StandAloneContext.startKoin
+import org.koin.standalone.StandAloneContext.stopKoin
+import org.koin.standalone.inject
+import org.koin.test.KoinTest
+
+class iTunesApiTest : KoinTest {
+
+    private val artistSearchQuery = "jack johnson"
+    private val artistIdQuery = 909253L
+    private val albumIdQuery = 879716730L
+
+    private val api by inject<iTunesSearchApi>()
+
+    @Before
+    fun `start koin`() {
+        startKoin(listOf(testModule))
+    }
+
+    @Test
+    fun `should get artists from iTunes api`() {
+        val result = runBlocking {
+            api.searchArtists(terms = artistSearchQuery).await()
+        }
+
+        assertNotNull(result)
+
+        val resultCount = result?.resultCount ?: 0
+
+        assertTrue(resultCount > 0)
+    }
+
+    @Test
+    fun `should get the artist's albums from iTunes api`() {
+        val result = runBlocking {
+            api.lookupAlbums(artistId = artistIdQuery).await()
+        }
+
+        assertNotNull(result)
+
+        val resultCount = result?.resultCount ?: 0
+
+        assertTrue(resultCount > 0)
+    }
+
+    @Test
+    fun `should get the album's songs from iTunes api`() {
+        val result = runBlocking {
+            api.lookupTracks(albumId = albumIdQuery).await()
+        }
+
+        assertNotNull(result)
+
+        val resultCount = result?.resultCount ?: 0
+
+        assertTrue(resultCount > 0)
+    }
+
+    @After
+    fun `stop koin`() {
+        stopKoin()
+    }
+}
