b8aadc6bbdbcf797ab54099da5c559f4bc7f3fd9
gdavidpb <gerardoprado93@gmail.com>
2019-04-15 04:26:42 -0400

    media player manager implemented

--- app/src/main/java/cl/yapo/test/ui/activities/AlbumDetailActivity.kt
+++ app/src/main/java/cl/yapo/test/ui/activities/AlbumDetailActivity.kt
@@ -1,8 +1,6 @@
 package cl.yapo.test.ui.activities
 
-import android.media.MediaPlayer
 import android.net.ConnectivityManager
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -20,7 +18,6 @@ import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.support.v4.onRefresh
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.io.File
 
 class AlbumDetailActivity : AppCompatActivity() {
 
@@ -30,7 +27,7 @@ class AlbumDetailActivity : AppCompatActivity() {
 
     private val trackAdapter = TrackAdapter(callback = TrackManager())
 
-    private var mediaPlayer: MediaPlayer? = null
+    private lateinit var mediaPlayerManager: MediaPlayerManager
 
     private val extraAlbumId by lazy {
         intent.getLongExtra(EXTRA_ALBUM_ID, 0)
@@ -69,12 +66,24 @@ class AlbumDetailActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        mediaPlayerManager = MediaPlayerManager(this)
+    }
+
     override fun onPause() {
-        super.onPause()
+        mediaPlayerManager.release()
 
         trackAdapter.resetStates()
 
-        resetPlayer()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        mediaPlayerManager.release()
+
+        super.onStop()
     }
 
     private fun tracksObserver(result: Result<List<Track>>?) {
@@ -116,62 +125,42 @@ class AlbumDetailActivity : AppCompatActivity() {
             is Result.OnSuccess -> {
                 val response = result.value
 
-                trackAdapter.updateItem(item = response.track) {
-                    copy(
-                        isPlaying = true,
-                        isPaused = false,
-                        isDownloading = false,
-                        isDownloaded = true
-                    )
-                }
+                val isReady = !response.track.isDownloading || response.track.isDownloaded
 
-                startPlayer(file = response.file)
+                if (isReady) {
+                    trackAdapter.updateItem(item = response.track) {
+                        copy(
+                            isPlaying = true,
+                            isPaused = false,
+                            isDownloading = false,
+                            isDownloaded = true
+                        )
+                    }
+
+                    mediaPlayerManager.play(source = response.file) {
+                        trackAdapter.updateItem(item = response.track) {
+                            copy(
+                                isPlaying = false,
+                                isPaused = false,
+                                isDownloading = false
+                            )
+                        }
+                    }
+                }
             }
             is Result.OnError -> {
-                trackAdapter.resetStates()
-
                 if (connectionManager.isNetworkAvailable())
                     longToast(R.string.toast_connection_failure)
                 else
                     longToast(R.string.toast_no_connection)
             }
-            else -> {
-                trackAdapter.resetStates()
-
-                longToast(R.string.toast_unexpected_failure)
-            }
         }
-    }
-
-    private fun resetPlayer() {
-        val previousPlayer = mediaPlayer
-
-        if (previousPlayer != null) {
-            if (previousPlayer.isPlaying) {
-                previousPlayer.setOnCompletionListener(null)
-
-                previousPlayer.stop()
-                previousPlayer.release()
-            }
-        }
-    }
-
-    private fun startPlayer(file: File) {
-        resetPlayer()
-
-        mediaPlayer = MediaPlayer.create(applicationContext, Uri.fromFile(file))
-
-        val newPlayer = mediaPlayer ?: return
-
-        newPlayer.setOnCompletionListener {
-            trackAdapter.resetStates()
-        }
-
-        newPlayer.start()
     }
 
     inner class TrackManager : TrackAdapter.AdapterCallback {
         override fun onPlayTrackClicked(track: Track, position: Int) {
+            mediaPlayerManager.stop()
+
             if (track.isDownloaded)
                 trackAdapter.updateItem(position = position) {
                     copy(
@@ -193,18 +182,14 @@ class AlbumDetailActivity : AppCompatActivity() {
         }
 
         override fun onPauseTrackClicked(track: Track, position: Int) {
-            val player = mediaPlayer ?: return
+            mediaPlayerManager.pause()
 
-            if (player.isPlaying) {
-                trackAdapter.updateItem(position = position) {
-                    copy(
-                        isPlaying = false,
-                        isPaused = true,
-                        isDownloading = false
-                    )
-                }
-
-                player.pause()
+            trackAdapter.updateItem(position = position) {
+                copy(
+                    isPlaying = false,
+                    isPaused = true,
+                    isDownloading = false
+                )
             }
         }
 
